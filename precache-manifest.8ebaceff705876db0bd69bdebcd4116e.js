729d8b92dd8479b275cd4a78ae9c7de97f044768
Eric <ericoneill34@gmail.com>
2018-11-27 18:06:56 -0500

    Updates

---
+++ precache-manifest.8ebaceff705876db0bd69bdebcd4116e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5841b5d326f014d08e7c960d38d3c283",
+    "url": "/sf/static/media/side-image.5841b5d3.jpg"
+  },
+  {
+    "revision": "995ca3cc826985b98b89303f841383f2",
+    "url": "/sf/static/media/profile-avatar.995ca3cc.jpeg"
+  },
+  {
+    "revision": "11c2da9d0856ceaa20b74c4ed4735c0b",
+    "url": "/sf/static/media/logo.11c2da9d.png"
+  },
+  {
+    "revision": "943df832bb66550a4e902a3d0bf02db9",
+    "url": "/sf/static/media/TMDBLogo.943df832.png"
+  },
+  {
+    "revision": "d3e1aabba859c0ff0f7e",
+    "url": "/sf/static/js/runtime~main.d3e1aabb.js"
+  },
+  {
+    "revision": "e50479e27291e3865781",
+    "url": "/sf/static/js/main.e50479e2.chunk.js"
+  },
+  {
+    "revision": "70bee3e81bf525086c40",
+    "url": "/sf/static/js/1.70bee3e8.chunk.js"
+  },
+  {
+    "revision": "e50479e27291e3865781",
+    "url": "/sf/static/css/main.9c1c5f78.chunk.css"
+  },
+  {
+    "revision": "5423be8c0de07c48053353615077a0fc",
+    "url": "/sf/index.html"
+  }
+];
